2919c4d62dc10f3328b7cde6dca772bb3369369a
Vincius <viniciustprates@gmail.com>
2021-03-29 17:21:49 -0300

    :construction: Created card with values

--- src/components/dasboard/index.js
+++ src/components/dasboard/index.js
@@ -1,15 +1,62 @@
 import React from 'react'
-import { Container, Titulo, Head } from './styles'
+import { 
+    Container, 
+    Titulo, 
+    Head, 
+    CardRow, 
+    ItemGeral, 
+    CardGeral, 
+    MiniCards, 
+    MiniCardWrapper, 
+    ItemFinancas } from './styles'
 import firebase from "gatsby-plugin-firebase"
 
 export default function Dashboard() {
+    
+    const [saldoTotal, setSaldoTotal] = React.useState()
     const [saldo, setSaldo] = React.useState()
-    const [despesas, setDespesas] = React.useState()
+    const [despesas, setDespesas] = React.useState() 
     const [credito, setCredito] = React.useState()
 
-    React.useEffect(() => {
+    const [saldoList, setSaldoList] = React.useState()
+    const [despesaList, setDespesaList] = React.useState()
+    const [creditoList, setCreditoList] = React.useState()
+
+    function calculaSaldoTotal(saldos, despesas) {
+        let s = saldos.reduce((acc, curr) => {
+            return acc + curr.valor
+        }, 0)
+        
+        let despesa = despesas.reduce((acc, curr) => {
+            return acc + curr.valor
+        }, 0)
+        let total = (s - despesa).toFixed(2)
+        setSaldoTotal(total)
+    }
+
+    function calculaSaldo() {
+        let s = saldoList.reduce((acc, curr) => {
+            return acc + curr.valor
+        }, 0)
+        setSaldo(s)
+    }
+    function calculaDespesa() {
+        let despesa = despesaList.reduce((acc, curr) => {
+            return acc + curr.valor
+        }, 0)
+        setDespesas(despesa)
+    }
+    function calculaCredito() {
+        let credit = creditoList.reduce((acc, curr) => {
+            return acc + curr.valor
+        }, 0)
+        console.log(creditoList)
+        setCredito(credit)
+    }
+
+    React.useEffect(async () => {
       const db = firebase.firestore()
-      db.collection("despesas").get().then((querySnapshot) => {
+      await db.collection("despesas").get().then((querySnapshot) => {
         let itens = []
         querySnapshot.forEach((doc) => {
             // console.log(`${doc.id} => ${doc.data()}`);
@@ -19,10 +66,10 @@ export default function Dashboard() {
           }) 
         });
 
-        console.log(itens)
-        setDespesas(itens)
+        //console.log(itens)
+        setDespesaList(itens)
       });
-      db.collection("saldo").get().then((querySnapshot) => {
+      await db.collection("saldo").get().then((querySnapshot) => {
         let itens = []
         querySnapshot.forEach((doc) => {
             // console.log(`${doc.id} => ${doc.data()}`);
@@ -32,10 +79,10 @@ export default function Dashboard() {
           }) 
         });
 
-        console.log(itens)
-        setSaldo(itens)
+        //console.log(itens)
+        setSaldoList(itens)
       });
-      db.collection("credito").get().then((querySnapshot) => {
+      await db.collection("credito").get().then((querySnapshot) => {
         let itens = []
         querySnapshot.forEach((doc) => {
             // console.log(`${doc.id} => ${doc.data()}`);
@@ -45,16 +92,52 @@ export default function Dashboard() {
           }) 
         });
 
-        console.log(itens)
-        setCredito(itens)
+        //console.log(itens)
+        setCreditoList(itens)
       });
+
     }, [])
+    
+    React.useEffect(() => {
+        if(saldoList && despesaList && creditoList) {
+            calculaSaldoTotal(saldoList, despesaList)
+            calculaCredito()
+            calculaDespesa()
+            calculaSaldo()
+        }
+    }, [saldoList, despesaList, creditoList])
     return (
         <Container>
             <Head>
                 <Titulo>Overview</Titulo>
             </Head>
-            
+            <CardRow>
+                <ItemGeral>
+                    <h3 >Geral</h3>
+                    <CardGeral>
+                        R${saldoTotal && saldoTotal}
+                    </CardGeral>
+                </ItemGeral>
+                <ItemFinancas>
+                    <h3 style={{marginLeft: 32}}>Suas financas</h3>
+                    
+                    <MiniCardWrapper>
+                    <MiniCards color={'#E5DEF0'}>
+                        Gasto
+                        <div>R${despesas}</div>
+                    </MiniCards>
+                    <MiniCards color={'#D6EDDA'}>
+                        Recebido
+                        <div>R${saldo}</div>
+                    </MiniCards>
+                    <MiniCards color={'#F6F0D8'}>
+                        Credito
+                        <div>R${credito}</div>
+                    </MiniCards>
+                    </MiniCardWrapper>
+                    
+                </ItemFinancas>
+            </CardRow>
         </Container>
     )
 }
--- src/components/dasboard/styles.js
+++ src/components/dasboard/styles.js
@@ -1,3 +1,4 @@
+import { symbol } from 'prop-types'
 import styled from 'styled-components'
 
 export const Container = styled.div`
@@ -7,6 +8,8 @@ export const Container = styled.div`
     border-radius: 45px;
     padding: 3rem;
     color: #000 !important;
+    display: flex;
+    flex-direction: column;
 `
 
 export const Titulo = styled.h1`
@@ -24,4 +27,53 @@ export const Titulo = styled.h1`
 export const Head = styled.header`
 
     margin: 0;
+`
+
+export const CardRow = styled.section`
+    min-height: 300px;
+    display: flex;
+`
+
+export const ItemGeral = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-weight: 600;
+    font-size: 27.5px;
+    width: 30%;
+`
+
+export const ItemFinancas = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-weight: 600;
+    font-size: 27.5px;
+    width: 70%;
+    max-height: 354px;
+`
+
+export const CardGeral = styled.div`
+    width: 100%;
+    height: 244px;
+
+    background: #E5F2FE;
+    border-radius: 24px;
+    text-align: center;
+`
+export const MiniCardWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    padding: 0;
+`
+export const MiniCards = styled.div`
+    width: 33%;
+    max-width: 194px;
+    height: 244px;
+
+    background: ${props => props.color};
+    border-radius: 24px;
+    margin-left: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    padding-left: 1rem;
 `
